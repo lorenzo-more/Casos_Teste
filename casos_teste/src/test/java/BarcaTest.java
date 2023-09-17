ad1e6c4688d0e12183b01b226b450ddba36da83f
Lucas Santos <schwartz.lucas@edu.pucrs.br>
2023-09-17 21:43:33 +0000

    entradas invalidas

--- casos_teste/src/test/java/BarcaTest.java
+++ casos_teste/src/test/java/BarcaTest.java
@@ -6,4 +6,27 @@ import org.junit.jupiter.api.Test;
 
 public class BarcaTest {
     
+    @BeforeEach
+    private Barca barca = new Barca();
+
+    // entradas inválidas
+    @teste
+    public void assentoInvalido() {
+        int result = barca.ocupaLugar("F20A21");
+        Assertions.assertEquals(0, result);
+    }
+
+    @teste
+    public void filaInvalida() {
+        int result = barca.ocupaLugar("F70A12");
+        Assertions.assertEquals(0, result);
+    }
+
+    @teste
+    public void entradaInvalida() {
+        int result = barca.ocupaLugar("A12F02");
+        Assertions.assertEquals(0, result);
+    }
+
+    
 }
