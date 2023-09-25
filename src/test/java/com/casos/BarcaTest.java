7441f58dcb750c8143f4f434b252958c625ef899
Mateus Poletto <pole@pop-os.localdomain>
2023-09-25 18:32:25 -0300

    Joga versão da barca arrumada

--- src/test/java/com/casos/BarcaTest.java
+++ src/test/java/com/casos/BarcaTest.java
@@ -4,14 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import net.bytebuddy.asm.Advice.Argument;
 
 // limites 
 // n de passageiros:
